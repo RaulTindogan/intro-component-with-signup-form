ec4587afab3859ed7c80895ffe9f5f965c877f36
RaulTindogan <tindoganraul@gmail.com>
2024-04-25 22:55:01 +0800

    created custom colors base on the style guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      "Red": "hsl(0, 100%, 74%)", 
+      "Green": "hsl(154, 59%, 51%)",
+      "Blue": "hsl(248, 32%, 49%)",
+      "Dark-Blue": "hsl(249, 10%, 26%)",
+      "Grayish-Blue": "hsl(246, 25%, 77%)"
+    },
+    fontFamily: {
+      "Poppins": ["Poppins", "sans-serif"]
+    },
     extend: {},
   },
   plugins: [],
